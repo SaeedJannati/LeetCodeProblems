3daa95e47e61fad2c74b319acfbea548b74a144c
Saeed Jannati <msaeedjannati72@gmail.com>
2024-08-24 00:13:23 +0330

    feat: add leet code 150 same tree

---
+++ LeetCodeProblemsCpp/LeetCodeProblemsCpp/Problems/LeetCode150SameTree.cpp
@@ -0,0 +1,29 @@
+﻿#include "LeetCode150SameTree.h"
+
+void LeetCode150SameTree::Run()
+{
+    
+}
+
+bool LeetCode150SameTree::isSameTree(TreeNode* p, TreeNode* q)
+{
+    if(p==nullptr && q==nullptr)
+        return true;
+    CheckIfSame(p,q);
+}
+bool LeetCode150SameTree::CheckIfSame(TreeNode* pNode,TreeNode* qNode)
+{
+    bool outPut=true;
+    if(pNode==nullptr ^ qNode==nullptr)
+        return false;
+    if(pNode==nullptr && qNode ==nullptr)
+        return true;
+    if(pNode->val != qNode->val)
+        return false;
+   outPut &= CheckIfSame(pNode->left,qNode->left);
+  outPut &=  CheckIfSame(pNode->right,qNode->right);
+    return outPut;
+}
+
+
+
---
+++ LeetCodeProblemsCpp/LeetCodeProblemsCpp/Problems/LeetCode150SameTree.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include "BaseProblem.h"
+
+struct TreeNode;
+
+class LeetCode150SameTree : BaseProblem
+{
+public:
+    void Run() override;
+
+private:
+    bool isSameTree(TreeNode* p, TreeNode* q);
+    bool CheckIfSame(TreeNode* pNode, TreeNode* qNode);
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr)
+    {
+    }
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr)
+    {
+    }
+
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right)
+    {
+    }
+};
