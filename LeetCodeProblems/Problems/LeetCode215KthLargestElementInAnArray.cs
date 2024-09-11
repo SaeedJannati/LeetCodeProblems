834a1e0ad477a1adff714f0942b6293073ba9323
Saeed Jannati <msaeedjannati72@gmail.com>
2024-09-11 20:52:49 +0330

    feat: add leet code 215 kth largest element c#

---
+++ LeetCodeProblems/Problems/LeetCode215KthLargestElementInAnArray.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeProblems.Problems;
+
+public class LeetCode215KthLargestElementInAnArray: BaseProblemClass
+{
+    public override void Run()
+    {
+        
+    }
+    public int FindKthLargest(int[] nums, int k)
+    {
+        var priorityQueue = new PriorityQueue<int,int>(nums.Length);
+        foreach (var num in nums)
+        {
+            priorityQueue.Enqueue(num, num);
+        }
+        for(int i=0;i<k-1;i++)
+            priorityQueue.Dequeue();
+        return priorityQueue.Dequeue();
+    }
+
+}
