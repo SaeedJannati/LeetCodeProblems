24deebc2c941cfd6c84f9905c53e57ae9e647b17
Saeed Jannati <msaeedjannati72@gmail.com>
2024-09-12 21:51:57 +0330

    feat: add leetcode133cloneGarph

---
+++ LeetCodeProblems/Problems/Leetcode133CloneGraph.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeProblems.Problems;
+
+public class Leetcode133CloneGraph
+{
+    Dictionary<int, Node> nodes = [];
+
+    public Node CloneGraph(Node node)
+    {
+        if (node == null)
+            return node;
+        return CloneNode(node);
+    }
+
+    Node CloneNode(Node node)
+    {
+        if (nodes.ContainsKey(node.val))
+            return nodes[node.val];
+        var aNode = new Node(node.val);
+        if (node.neighbors == null)
+            return aNode;
+        if (node.neighbors.Count == 0)
+            return aNode;
+        aNode.neighbors = new List<Node>(node.neighbors.Count);
+
+        nodes[aNode.val] = aNode;
+        for (int i = 0, e = node.neighbors.Count; i < e; i++)
+        {
+            aNode.neighbors.Add(CloneNode(node.neighbors[i]));
+        }
+
+        return aNode;
+    }
+}
