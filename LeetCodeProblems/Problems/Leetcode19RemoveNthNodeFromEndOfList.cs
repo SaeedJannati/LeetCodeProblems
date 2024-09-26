f1d7e737c7cfd14b32e9d65d1aa9eb9199c6525a
Saeed Jannati <msaeedjannati72@gmail.com>
2024-09-26 20:02:23 +0330

    feat: add leetcode 19 remove Nth node from end of list

---
+++ LeetCodeProblems/Problems/Leetcode19RemoveNthNodeFromEndOfList.cs
@@ -0,0 +1,24 @@
+﻿using LeetCodeProblems.Models;
+
+namespace LeetCodeProblems.Problems;
+
+public class Leetcode19RemoveNthNodeFromEndOfList
+{
+    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+        Dictionary<int, ListNode> nodesWithIndex = [];
+        var currentNode = head;
+        var index = 0;
+        while (currentNode!=null)
+        {
+            nodesWithIndex[index] = currentNode;
+            currentNode = currentNode.next;
+            index++;
+        }
+
+        if (nodesWithIndex[index-n] == head)
+            return head.next;
+        nodesWithIndex[index-n-1].next=nodesWithIndex[index-n-1].next.next;
+        return head;
+    }
+}
