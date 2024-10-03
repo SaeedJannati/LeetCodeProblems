5440ae26d997129baa2dd0c27b9b0d33ad09226d
Saeed Jannati <msaeedjannati72@gmail.com>
2024-10-03 20:48:33 +0330

    feat: add LeetCode70ClimbingStairs

---
+++ LeetCodeProblemsCpp/LeetCodeProblemsCpp/Problems/LeetCode70ClimbingStairs.cpp
@@ -0,0 +1,26 @@
+﻿#include "LeetCode70ClimbingStairs.h"
+
+#include <vector>
+
+void LeetCode70ClimbingStairs::Run()
+{
+    BaseProblem::Run();
+}
+
+int LeetCode70ClimbingStairs::climbStairs(int n)
+{
+    if(n==0)
+        return 0;
+    if(n==1)
+        return 1;
+    std::vector<int> dpMatrix(n);
+    dpMatrix.reserve(n);
+    dpMatrix[0] = 1;
+    dpMatrix[1]=2;
+    for (int i=2;i<n;i++)
+    {
+        dpMatrix[i]=dpMatrix[i-1]+dpMatrix[i-2];
+    }
+    return dpMatrix[n-1];
+    
+}
---
+++ LeetCodeProblemsCpp/LeetCodeProblemsCpp/Problems/LeetCode70ClimbingStairs.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "BaseProblem.h"
+
+class LeetCode70ClimbingStairs: BaseProblem
+{
+public:
+    void Run() override;
+private:
+    int climbStairs(int n);
+};
