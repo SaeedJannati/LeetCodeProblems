093b7c36307b3704b2f7e824e91be21ebe79990e
Saeed Jannati <msaeedjannati72@gmail.com>
2024-10-25 12:09:14 +0330

    feat: add LeetCode49GroupAnagrams

---
+++ LeetCodeProblems/Problems/LeetCode49GroupAnagrams.cs
@@ -0,0 +1,68 @@
+﻿namespace LeetCodeProblems.Problems;
+
+public class LeetCode49GroupAnagrams : BaseProblemClass
+{
+    public override void Run()
+    {
+    }
+
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        Dictionary<Dictionary<char, int>, Queue<string>> dict = [];
+        foreach (var str in strs)
+        {
+            var vector = GetVector(str);
+            bool foundPlace = false;
+            foreach (var pair in dict)
+            {
+                if (AreAnagrams(vector, pair.Key))
+                {
+                    pair.Value.Enqueue(str);
+                    foundPlace = true;
+                    break;
+                }
+            }
+
+            if (!foundPlace)
+            {
+                dict[vector] = new Queue<string>();
+                dict[vector].Enqueue(str);
+            }
+            
+        }
+
+        return dict.Values.Select(queue => queue.ToList()).Cast<IList<string>>().ToList();
+    }
+
+    private bool AreAnagrams(Dictionary<char, int> dic1, Dictionary<char, int> dic2)
+    {
+        if (dic1.Count != dic2.Count)
+            return false;
+        foreach (var pair in dic1)
+        {
+            if (!dic2.TryGetValue(pair.Key, out var value))
+                return false;
+            if (value != pair.Value)
+                return false;
+        }
+
+        return true;
+    }
+
+    private Dictionary<char, int> GetVector(string word)
+    {
+        Dictionary<char, int> vector = [];
+        foreach (char c in word)
+        {
+            if (vector.ContainsKey(c))
+            {
+                vector[c]++;
+                continue;
+            }
+
+            vector[c] = 1;
+        }
+
+        return vector;
+    }
+}
