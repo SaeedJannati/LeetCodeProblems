5415bf0ad3a394bca493ad4b41af5526c391b9b0
Saeed Jannati <msaeedjannati72@gmail.com>
2024-11-01 18:18:44 +0330

    feat: add LeetCode58LengthOfLastWord

---
+++ LeetCodeProblems/Problems/LeetCode58LengthOfLastWord.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCodeProblems.Problems;
+
+public class LeetCode58LengthOfLastWord
+{
+    public int LengthOfLastWord(string s)
+    {
+        var words = s.Split(' ');
+        string current = "";
+        for (int i = words.Length-1; i >= 0; i--)
+        {
+            current = words[i].Replace(" ","");
+            if(current.Length>0)
+                return current.Length;
+        }
+
+        return 0;
+    }
+}
