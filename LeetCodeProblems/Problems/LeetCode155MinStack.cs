f3c241af76160e22c8defa1fd02af1cf5a7d920c
Saeed Jannati <msaeedjannati72@gmail.com>
2024-11-01 17:55:09 +0330

    feat: add LeetCode155MinStack

---
+++ LeetCodeProblems/Problems/LeetCode155MinStack.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCodeProblems.Problems;
+
+public class LeetCode155MinStack
+{
+    private List<int> list=[];
+    SortedList<int,int> sortedList=[];
+    private Dictionary<int, int> dict = [];
+    public MinStack() {
+        
+    }
+    
+    public void Push(int val) {
+        list.Add(val);
+        if (dict.ContainsKey(val))
+        {
+            dict[val]++;
+            return;
+        }
+
+        dict[val] = 1;
+        sortedList.Add(val,val);
+    }
+    
+    public void Pop()
+    {
+        var value = list[^1];
+        list.RemoveAt(list.Count - 1);
+        dict[value]--;
+        if (dict[value] == 0)
+        {
+            dict.Remove(value);
+            sortedList.Remove(value);
+        }
+
+    }
+    
+    public int Top()
+    {
+        return list[^1];
+    }
+    
+    public int GetMin()
+    {
+        return sortedList.First().Value;
+    }
+}
