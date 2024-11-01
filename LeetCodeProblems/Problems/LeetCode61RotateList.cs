747adeb739b5e878343d134809b4c558a4478098
Saeed Jannati <msaeedjannati72@gmail.com>
2024-11-01 17:39:08 +0330

    feat: add LeetCode61RotateList

---
+++ LeetCodeProblems/Problems/LeetCode61RotateList.cs
@@ -0,0 +1,54 @@
+﻿using LeetCodeProblems.Models;
+
+namespace LeetCodeProblems.Problems;
+
+public class LeetCode61RotateList : BaseProblemClass
+{
+    public ListNode RotateRight(ListNode head, int k)
+    {
+        if (head == null || head.next == null)
+            return head;
+        int counter = 0;
+        ListNode current = head;
+        ListNode previous = null;
+        while (current != null)
+        {
+            previous=current;
+            current = current.next;
+            counter++;
+        }
+
+        if (k >= counter)
+            k %= counter;
+        if (k == 0)
+            return head;
+        var output = GetTail(head, counter-k);
+        output.tailPrev.next = null;
+        previous.next = head;
+        return output.tail;
+    }
+
+    private (ListNode tail, ListNode tailPrev) GetTail(ListNode root, int k)
+    {
+        int counter = 0;
+        ListNode current = root;
+        ListNode previous = null;
+        while (counter < k && current != null)
+        {
+            previous = current;
+            current = current.next;
+            counter++;
+        }
+
+        return (current, previous);
+    }
+
+    public override void Run()
+    {
+        List<int> input = [1, 2];
+        int k = 2;
+        var head = ListNode.CreateLinkedLis(input);
+        var newHead = RotateRight(head, k);
+        ListNode.PrintList(newHead);
+    }
+}
--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -3,7 +3,7 @@
 using LeetCodeProblems.Auxilaries;
 using LeetCodeProblems.Problems;
 
-var problem = new LeetCode25ReverseNodesInKGroup();
+var problem = new LeetCode61RotateList();
 
 problem.Run();
 
