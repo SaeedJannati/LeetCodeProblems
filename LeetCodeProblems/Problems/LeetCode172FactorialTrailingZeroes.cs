722a3bae754e6e538e6d806c443e5175fb42bed2
Saeed Jannati <msaeedjannati72@gmail.com>
2024-11-01 21:47:58 +0330

    feat: add LeetCode172FactorialTrailingZeroes

---
+++ LeetCodeProblems/Problems/LeetCode172FactorialTrailingZeroes.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCodeProblems.Problems;
+
+public class LeetCode172FactorialTrailingZeroes
+{
+    public int TrailingZeroes(int n)
+    {
+       var largestPowerOfFive= (int)(Math.Log(n)/Math.Log(5));
+       int counter = 0;
+       for (int i = 1; i <= largestPowerOfFive; i++)
+       {
+           counter+=(int)(n/Math.Pow(5, i));
+       }
+       return counter;
+    }
+}
