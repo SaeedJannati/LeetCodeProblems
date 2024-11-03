2f9f0b85fedde186a4b1f5118fe82cf09d6449ed
Saeed Jannati <msaeedjannati72@gmail.com>
2024-11-03 14:57:42 +0330

    fix: solve LeetCode121BestTimeToBuyAndSellStock in O(n)

--- LeetCodeProblems/Problems/LeetCode121BestTimeToBuyAndSellStock.cs
+++ LeetCodeProblems/Problems/LeetCode121BestTimeToBuyAndSellStock.cs
@@ -4,36 +4,53 @@ public class LeetCode121BestTimeToBuyAndSellStock : BaseProblemClass
 {
     public int MaxProfit(int[] prices)
     {
-        var results = new SortedList<int, int>(prices.Length);
-        Dictionary<int, int> frequencies = new Dictionary<int, int>(prices.Length);
-        foreach (var price in prices)
+        int bestBuyPrice = prices[0];
+        int mostProfit = 0;
+        int profit = 0;
+        for (int i = 1,e=prices.Length; i < e; i++)
         {
-            
-            if (frequencies.ContainsKey(price))
-                frequencies[price]++;
-            else
-            {
-                frequencies[price] = 1;
-                results.Add(-price, price);
-            }
-
-            
+            profit= prices[i]-bestBuyPrice;
+            if(profit > mostProfit)
+                mostProfit=profit;
+            if(prices[i]<bestBuyPrice)
+                bestBuyPrice = prices[i];
         }
-
-        int delta = 0;
-        int maxSoFar = 0;
-        for (int i = 0, e = prices.Length - 1; i < e; i++)
-        {
-            frequencies[prices[i]]--;
-            if (frequencies[prices[i]] == 0)
-                results.Remove(-prices[i]);
-            delta = results.First().Value - prices[i];
-            if (delta > maxSoFar)
-                maxSoFar = delta;
-        }
-
-        return maxSoFar;
+        return mostProfit;
     }
+
+    // nlogN
+    // public int MaxProfit(int[] prices)
+    // {
+    //     var results = new SortedList<int, int>(prices.Length);
+    //     Dictionary<int, int> frequencies = new Dictionary<int, int>(prices.Length);
+    //     foreach (var price in prices)
+    //     {
+    //         
+    //         if (frequencies.ContainsKey(price))
+    //             frequencies[price]++;
+    //         else
+    //         {
+    //             frequencies[price] = 1;
+    //             results.Add(-price, price);
+    //         }
+    //
+    //         
+    //     }
+    //
+    //     int delta = 0;
+    //     int maxSoFar = 0;
+    //     for (int i = 0, e = prices.Length - 1; i < e; i++)
+    //     {
+    //         frequencies[prices[i]]--;
+    //         if (frequencies[prices[i]] == 0)
+    //             results.Remove(-prices[i]);
+    //         delta = results.First().Value - prices[i];
+    //         if (delta > maxSoFar)
+    //             maxSoFar = delta;
+    //     }
+    //
+    //     return maxSoFar;
+    // }
 
     public override void Run()
     {
