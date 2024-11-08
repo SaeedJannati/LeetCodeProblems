e4acf5394efd4812460c6b90b4a8d2158e6a5789
Saeed Jannati <msaeedjannati72@gmail.com>
2024-11-08 11:15:37 +0330

    feat: add LeetCode137SingleNumberII

---
+++ LeetCodeProblems/Models/Trie.cs
@@ -0,0 +1,58 @@
+﻿namespace LeetCodeProblems.Models;
+
+public class Trie
+{
+    private readonly TrieNode _root=new ();
+    public void Insert(string word)
+    {
+        var current = _root;
+        TrieNode newNode = new();
+        foreach (var c in word)
+        {
+            if (current.Children.TryGetValue(c, out var child))
+            {
+                current = child;
+                continue;
+            }
+
+            newNode = new TrieNode();
+            current.Children.Add(c, newNode);
+            current = current.Children[c];
+        }
+
+        current.IsEndOfTerm = true;
+    }
+
+    public bool Search(string word)
+    {
+        var current = _root;
+        foreach (var c in word)
+        {
+            if (!current.Children.TryGetValue(c, value: out var child))
+                return false;
+            current = child;
+        }
+
+        return current.IsEndOfTerm;
+    }
+
+    public bool StartsWith(string prefix)
+    {
+        var current = _root;
+        foreach (var c in prefix)
+        {
+            if (!current.Children.TryGetValue(c, out var value))
+                return false;
+            current = value;
+        }
+
+        return true;
+    }
+
+
+    public class TrieNode
+    {
+        public  Dictionary<char, TrieNode> Children { get; } = [];
+        public bool IsEndOfTerm { get; set; }
+    }
+}
---
+++ LeetCodeProblems/Problems/LeetCode137SingleNumberII.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeProblems.Problems;
+
+public class LeetCode137SingleNumberII
+{
+    public int SingleNumber(int[] nums)
+    {
+        int length = nums.Length;
+        HashSet<int> numsAppearedMoreThanOnce = new(length);
+        HashSet<int> numsAppearedOnce = [];
+        for (int i = 0; i < length; i++)
+        {
+            if(numsAppearedMoreThanOnce.Contains(nums[i]))
+                continue;
+            if (numsAppearedOnce.Contains(nums[i]))
+            {
+                numsAppearedMoreThanOnce.Add(nums[i]);
+                numsAppearedOnce.Remove(nums[i]);
+                continue;
+            }
+            numsAppearedOnce.Add(nums[i]);
+        }
+
+        return numsAppearedOnce.First();
+    }
+}
