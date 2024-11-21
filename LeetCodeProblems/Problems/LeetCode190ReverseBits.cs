729b930c50978e40d977c4bd2188e7020aebc228
Saeed Jannati <msaeedjannati72@gmail.com>
2024-11-21 23:43:55 +0330

    feat: add LeetCode190ReverseBits

---
+++ LeetCodeProblems/Problems/LeetCode190ReverseBits.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeProblems.Problems;
+
+public class LeetCode190ReverseBits
+{
+    public uint reverseBits(uint n)
+    {
+        Stack<bool> binary = [];
+
+        for (int i = 0; i < 32; i++)
+        {
+            binary.Push(n % 2 == 1);
+            n /= 2;
+        }
+
+        uint result = 0;
+        uint coefficient = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            coefficient=(uint)(binary.Pop()? 1 : 0);
+            if(coefficient == 0)
+                continue;
+            result += coefficient*(uint)Math.Pow(2, i);
+        }
+
+        return result;
+    }
+}
