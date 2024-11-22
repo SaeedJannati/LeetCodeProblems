ae055e816efcdd491b900ec51fa7b6d67b31b599
Saeed Jannati <msaeedjannati72@gmail.com>
2024-11-22 22:26:25 +0330

    feat: add LeetCode2390RemovingStarsFromaString

---
+++ LeetCodeProblems/Problems/LeetCode2390RemovingStarsFromaString.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeProblems.Problems;
+
+public class LeetCode2390RemovingStarsFromaString
+{
+    public string RemoveStars(string s)
+    {
+        if (s == null || s.Length == 0)
+            return s;
+        Stack<char> stack = [];
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '*')
+            {
+                stack.Pop();
+                continue;
+            }
+
+            stack.Push(s[i]);
+        }
+        List<char> result = new List<char>(stack.Count);
+        while (stack.Count>0)
+        {
+            result.Add(stack.Pop());
+        }
+        result.Reverse();
+        return  new string(result.ToArray());
+    }
+}
