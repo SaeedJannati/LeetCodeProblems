95bf1ebc9c9117b2fb8baae963c9d5328ae174ce
Saeed Jannati <msaeedjannati72@gmail.com>
2024-11-22 22:19:32 +0330

    feat: add LeetCode1456MaximumNumberofVowelsinaSubstringofGivenLength

---
+++ LeetCodeProblems/Problems/LeetCode1456MaximumNumberofVowelsinaSubstringofGivenLength.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCodeProblems.Problems;
+
+public class LeetCode1456MaximumNumberofVowelsinaSubstringofGivenLength
+{
+    public int MaxVowels(string s, int k)
+    {
+        if (s == null || s.Length == 0)
+            return 0;
+        int maxVowels = 0;
+        int currentVowels = 0;
+        int length = s.Length;
+        int top = Math.Min(length, k);
+        for (int i = 0; i < top; i++)
+        {
+            if(IsVowel(s[i]))
+                maxVowels++;
+        }
+        if(k>=length)
+            return maxVowels;
+        currentVowels = maxVowels;
+        for (int i = top ; i < length; i++)
+        {
+            if(IsVowel(s[i-k]))
+                currentVowels--;
+            if(IsVowel(s[i]))
+                currentVowels++;
+            if(currentVowels>maxVowels)
+                maxVowels = currentVowels;
+        }
+
+        return maxVowels;
+    }
+
+    private bool IsVowel(char c)
+    {
+        return c is 'a' or 'e' or 'i' or 'o' or 'u';
+    }
+
+}
