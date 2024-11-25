45b1c35ad755b99022ecad90d7bdfffdc776ad71
Saeed Jannati <msaeedjannati72@gmail.com>
2024-11-25 22:24:02 +0330

    feat: add LeetCode875KokoEatingBananas

---
+++ LeetCodeProblems/Problems/LeetCode875KokoEatingBananas.cs
@@ -0,0 +1,57 @@
+﻿namespace LeetCodeProblems.Problems;
+
+public class LeetCode875KokoEatingBananas : BaseProblemClass
+{
+    public int MinEatingSpeed(int[] piles, int h)
+    {
+        Array.Sort(piles);
+        if (h == piles.Length)
+            return piles[^1];
+        if (piles.Length == 1)
+        {
+            if (h > piles[^1])
+                return 1;
+            return piles[0]/h   + (piles[0] %h  == 0 ? 0 : 1);
+        }
+
+        int begin = 1;
+        int end = piles[^1];
+        int mid = 0;
+        int spent = 0;
+        int min = int.MaxValue;
+        while (begin <= end && begin > 0)
+        {
+            mid = (begin + end) / 2;
+            spent = 0;
+            foreach (var t in piles)
+            {
+                spent += t / mid;
+                if (t % mid != 0)
+                    spent++;
+                if (spent > h)
+                    break;
+            }
+
+            if (spent <= h)
+                if (mid < min)
+                    min = mid;
+            if (spent > h)
+            {
+                begin = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+
+        return min;
+    }
+
+    public override void Run()
+    {
+        int[] input = [312884470];
+        int h = 312884469;
+        Console.WriteLine(MinEatingSpeed(input, h));
+    }
+}
--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -3,7 +3,7 @@
 using LeetCodeProblems.Auxilaries;
 using LeetCodeProblems.Problems;
 
-var problem = new LeetCode216CombinationSumIII();
+var problem = new LeetCode875KokoEatingBananas();
 
 problem.Run();
 
