97029f7ac4784ee41866bbf56eb7a5e20e25b823
Saeed Jannati <msaeedjannati72@gmail.com>
2024-11-25 23:22:27 +0330

    feat: add LeetCode1431KidsWiththeGreatestNumberofCandies

---
+++ LeetCodeProblems/Problems/LeetCode1431KidsWiththeGreatestNumberofCandies.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCodeProblems.Problems;
+
+public class LeetCode1431KidsWiththeGreatestNumberofCandies
+{
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies) 
+    {
+        int maxCandies = int.MinValue;
+        int length=candies.Length;
+        for (int i = 0; i < length; i++)
+        {
+            if(candies[i] > maxCandies)
+                maxCandies = candies[i];
+        }
+        IList<bool> result=new List<bool>(length);
+        for (int i = 0; i < length; i++)
+        {
+            result.Add(candies[i] +extraCandies>=maxCandies);
+        }
+        return result;
+    }
+    
+    //Using C# methods
+    // public IList<bool> KidsWithCandies(int[] candies, int extraCandies) 
+    // {
+    //     int maxCandies = candies.Max();
+    //     IList<bool> result = candies.Select(i=>i+extraCandies>=maxCandies).ToList();
+    //     return result;
+    // }
+}
