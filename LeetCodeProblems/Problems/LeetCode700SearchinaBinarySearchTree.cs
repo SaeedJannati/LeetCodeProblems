578fa8e3bf563bb64004057cf3aeea8ace754734
Saeed Jannati <msaeedjannati72@gmail.com>
2024-11-28 18:37:31 +0330

    feat: add LeetCode700SearchinaBinarySearchTree

---
+++ LeetCodeProblems/Problems/LeetCode700SearchinaBinarySearchTree.cs
@@ -0,0 +1,24 @@
+﻿using LeetCodeProblems.Models;
+
+namespace LeetCodeProblems.Problems;
+
+public class LeetCode700SearchinaBinarySearchTree
+{
+    public TreeNode SearchBST(TreeNode root, int val) {
+        return Find(root, val);
+    }
+
+    private TreeNode? Find(TreeNode root, int val)
+    {
+        if(val == root.val)
+            return root;
+        if (val < root.val)
+        {
+            if(root.left != null)
+                return Find(root.left, val);
+        }
+        if(root.right != null)
+            return Find(root.right, val);
+        return null;
+    }
+}
