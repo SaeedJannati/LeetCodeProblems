7ba0077e2d9c30accfa3389077a480047ba9fc76
Saeed Jannati <msaeedjannati72@gmail.com>
2024-12-05 19:58:25 +0330

    feat: add LeetCode1768MergeStringsAlternately

---
+++ LeetCodeProblems/Problems/LeetCode1768MergeStringsAlternately.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace LeetCodeProblems.Problems;
+
+public class LeetCode1768MergeStringsAlternately : BaseProblemClass
+{
+    public string MergeAlternately(string word1, string word2)
+    {
+        int fisrtLength = word1.Length;
+        int secondLength = word2.Length;
+        int maxLength = Math.Max(fisrtLength, secondLength);
+        StringBuilder builder = new();
+        for (int i = 0; i < maxLength; i++)
+        {
+            if(i<fisrtLength)
+                builder.Append(word1[i]);
+            if(i<secondLength)
+                builder.Append(word2[i]);
+        }
+
+        return builder.ToString();
+    }
+
+    public override void Run()
+    {
+        string fisrtWord = "ab";
+        string secondWord = "pqrs";
+        Console.WriteLine(MergeAlternately(fisrtWord, secondWord));
+    }
+}
--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -3,7 +3,7 @@
 using LeetCodeProblems.Auxilaries;
 using LeetCodeProblems.Problems;
 
-var problem = new LeetCode1372LongestZigZagPathinaBinaryTree();
+var problem = new LeetCode1768MergeStringsAlternately();
 
 problem.Run();
 
