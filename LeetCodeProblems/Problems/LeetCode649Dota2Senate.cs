22f273e9167c5d9e239dd300929c2ec666be610b
Saeed Jannati <msaeedjannati72@gmail.com>
2024-12-05 18:51:00 +0330

    feat: add LeetCode649Dota2Senate

---
+++ LeetCodeProblems/Problems/LeetCode649Dota2Senate.cs
@@ -0,0 +1,59 @@
+﻿namespace LeetCodeProblems.Problems;
+
+public class LeetCode649Dota2Senate
+{
+    public string PredictPartyVictory(string senate)
+    {
+        int direCount = 0;
+        int radiantCount = 0;
+        int direVoteCount = 0;
+        int radiantVoteCount = 0;
+        Queue<char> queue = new Queue<char>();
+        foreach (var c in senate)
+        {
+            if (c == 'R')
+            {
+                radiantCount++;
+            }
+            else
+            {
+                direCount++;
+            }
+            queue.Enqueue(c);
+            
+        }
+
+        char current;
+        while (direCount>0 && radiantCount>0)
+        {
+            current=queue.Dequeue();
+            if (current == 'R')
+            {
+                if (direVoteCount > 0)
+                {
+                    direVoteCount--;
+                    radiantCount--;
+                    if (radiantCount == 0)
+                        return "Dire";
+                    continue;
+                }
+                radiantVoteCount++;
+            }
+            else
+            {
+                if (radiantVoteCount > 0)
+                {
+                    radiantVoteCount--;
+                    direCount --;
+                    if (direCount == 0)
+                        return "Radiant";
+                    continue;
+                }
+                direVoteCount ++;
+            }
+
+            queue.Enqueue(current);
+        }
+        return direCount>0? "Dire" : "Radiant";
+    }
+}
