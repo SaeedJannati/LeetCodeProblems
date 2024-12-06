02ee40c816508002703a75347ea18d7c9f3c7660
Saeed Jannati <msaeedjannati72@gmail.com>
2024-12-06 13:48:36 +0330

    feat: add LeetCode374GuessNumberHigherorLower

---
+++ LeetCodeProblems/Problems/LeetCode374GuessNumberHigherorLower.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeProblems.Problems;
+
+public class LeetCode374GuessNumberHigherorLower
+{
+
+    private int guess(int num) => 0;
+    public int GuessNumber(int n)
+    {
+        long begin = 0;
+        long end = n;
+        int mid = 0;
+        while (begin <= end)
+        {
+            mid=(int)((begin + end) / 2);
+            if (guess(mid) == 0)
+                return mid;
+            if(guess(mid)==1)
+                begin = mid + 1;
+            else
+                end = mid - 1;
+        }
+
+        return mid;
+    }
+
+
+}
