cbcfc0da3160b1f7af889bab8dd0b71c229386fe
Saeed Jannati <msaeedjannati72@gmail.com>
2024-12-12 19:19:30 +0330

    feat: add LeetCode347TopKFrequentElements

---
+++ LeetCodeProblems/Problems/LeetCode347TopKFrequentElements.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCodeProblems.Problems;
+
+public class LeetCode347TopKFrequentElements
+{
+    public int[] TopKFrequent(int[] nums, int k)
+    {
+        Dictionary<int, int> occurrences = [];
+        int length = nums.Length;
+        for (int i = 0; i < length; i++)
+        {
+            if (occurrences.TryGetValue(nums[i], out var value))
+                occurrences[nums[i]] = ++value;
+            else
+                occurrences[nums[i]] = 1;
+        }
+
+        PriorityQueue<int, int> heap = new(occurrences.Count);
+        foreach (var pair in occurrences)
+        {
+            heap.Enqueue(pair.Key, -pair.Value);
+        }
+
+        var result = new int[k];
+        for (int i = 0; i < k; i++)
+        {
+            result[i]= heap.Dequeue();
+        }
+        return result;
+    }
+}
