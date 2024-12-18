614a1d795139bd3a9e80bec82b2bea93136a45d9
Saeed Jannati <msaeedjannati72@gmail.com>
2024-12-18 13:58:57 +0330

    feat: add LeetCode239SlidingWindowMaximum

--- LeetCodeProblems/Problems/LeetCode239SlidingWindowMaximum.cs
+++ LeetCodeProblems/Problems/LeetCode239SlidingWindowMaximum.cs
@@ -1,34 +1,86 @@
+using System.Text.Json;
+
 namespace LeetCodeProblems.Problems;
 
-public class LeetCode239SlidingWindowMaximum
+public class LeetCode239SlidingWindowMaximum: BaseProblemClass
 {
-    public int[] MaxSlidingWindow(int[] nums, int k)
-    {
-        SortedDictionary<int, int> window = [];
-        int length = nums.Length;
-        if (window.Count == 0)
+    #region Order n
+
+    public int[] MaxSlidingWindow(int[] nums, int k) {
+        int length=nums.Length;
+        int[] leftMax = new int[length];
+        int[] rightMax = new int[length];
+        leftMax[0]=nums[0];
+        rightMax[length-1]=nums[length-1];
+        int[] result = new int [length - k + 1];
+        for (int i = 1; i < length; i++)
         {
-            for (int i = 0; i < k; i++)
+            if (i % k == 0)
             {
-                if (!window.TryAdd(-nums[i], 1))
-                    window[-nums[i]]++;
+                leftMax[i]=nums[i];
+            }
+            else
+            {
+                leftMax[i]=Math.Max(leftMax[i-1], nums[i]);
+            }
+            if ((length-i-1) % k == 0)
+            {
+                rightMax[length-1-i]=nums[length-1-i];
+            }
+            else
+            {
+                rightMax[length-1-i]=Math.Max(rightMax[length-i], nums[length-1-i]);
             }
         }
 
-        int[] result = new int[length - k + 1];
-        result[0] = -window.First().Key;
-        int counter = 1;
-        for (int i = k; i < length; i++)
+        for (int i = 0; i < length-k+1; i++)
         {
-            window[-nums[i - k]]--;
-            if(window[-nums[i - k]] == 0)
-                window.Remove(-nums[i - k]);
-            if (!window.TryAdd(-nums[i], 1))
-                window[-nums[i]]++;
-            result[counter] = -window.First().Key;
-            counter++;
+            result[i] = Math.Max(rightMax[i], leftMax[i + k-1]);
         }
 
         return result;
     }
+    public override void Run()
+    {
+        int[] nums = [1,3,-1,-3,5,3,6,7];
+        int k = 3;
+        Console.WriteLine(JsonSerializer.Serialize(MaxSlidingWindow(nums, k)));
+    }
+    #endregion
+    #region O nLog(k)
+
+    // public int[] MaxSlidingWindow(int[] nums, int k)
+    // {
+    //     SortedDictionary<int, int> window = [];
+    //     int length = nums.Length;
+    //     if (window.Count == 0)
+    //     {
+    //         for (int i = 0; i < k; i++)
+    //         {
+    //             if (!window.TryAdd(-nums[i], 1))
+    //                 window[-nums[i]]++;
+    //         }
+    //     }
+    //
+    //     int[] result = new int[length - k + 1];
+    //     result[0] = -window.First().Key;
+    //     int counter = 1;
+    //     for (int i = k; i < length; i++)
+    //     {
+    //         window[-nums[i - k]]--;
+    //         if(window[-nums[i - k]] == 0)
+    //             window.Remove(-nums[i - k]);
+    //         if (!window.TryAdd(-nums[i], 1))
+    //             window[-nums[i]]++;
+    //         result[counter] = -window.First().Key;
+    //         counter++;
+    //     }
+    //
+    //     return result;
+    // }
+
+    #endregion
+
+
 }
+
--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -3,7 +3,7 @@
 using LeetCodeProblems.Auxilaries;
 using LeetCodeProblems.Problems;
 
-var problem = new LeetCode152MaximumProductSubarray();
+var problem = new LeetCode239SlidingWindowMaximum();
 
 problem.Run();
 
