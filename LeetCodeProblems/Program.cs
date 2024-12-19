43b4340ce1fc29e2b815d5d631c764027ed387d8
Saeed Jannati <msaeedjannati72@gmail.com>
2024-12-19 19:54:20 +0330

    feat: add LeetCode279PerfectSquares

--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -3,7 +3,7 @@
 using LeetCodeProblems.Auxilaries;
 using LeetCodeProblems.Problems;
 
-var problem = new LeetCode279PerfectSquares();
+var problem = new LeetCode84LargestRectangleinHistogram();
 
 problem.Run();
 
