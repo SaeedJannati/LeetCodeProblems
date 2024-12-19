415cab5fe0e4cd1eff9e2f454c3d9d32b9e2be3b
Saeed Jannati <msaeedjannati72@gmail.com>
2024-12-19 21:56:51 +0330

    feat: add LeetCode234PalindromeLinkedList

---
+++ LeetCodeProblems/Problems/LeetCode234PalindromeLinkedList.cs
@@ -0,0 +1,30 @@
+﻿using LeetCodeProblems.Models;
+
+namespace LeetCodeProblems.Problems;
+
+public class LeetCode234PalindromeLinkedList
+{
+    public bool IsPalindrome(ListNode head)
+    {
+        if (head == null )
+            return false;
+        if (head.next == null)
+            return true;
+        Stack<int> stack = new();
+        Queue<int> queue = new();
+        ListNode node = head;
+        while (node is not null)
+        {
+            stack.Push(node.val);
+            queue.Enqueue(node.val);
+            node=node.next;
+        }
+
+        while (stack.Count > 0)
+        {
+            if(stack.Pop()!=queue.Dequeue())
+                return false;
+        }
+        return true;
+    }
+}
