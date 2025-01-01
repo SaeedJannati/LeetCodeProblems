305432c85bf8b3db03afddb29655c89056ffd21e
Saeed Jannati <msaeedjannati72@gmail.com>
2025-01-01 23:05:17 +0330

    feat: add vscode cpp project

---
+++ vscodecppapp/build/CMakeFiles/CPPConsoleApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPPConsoleApp.dir/main.cpp.o"
+  "CMakeFiles/CPPConsoleApp.dir/main.cpp.o.d"
+  "CPPConsoleApp"
+  "CPPConsoleApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPPConsoleApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ vscodecppapp/build/CMakeFiles/CPPConsoleApp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CPPConsoleApp.
+# This may be replaced when dependencies are built.
