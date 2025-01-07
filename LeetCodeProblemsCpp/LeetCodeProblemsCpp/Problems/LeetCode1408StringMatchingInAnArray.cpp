b117fe866a70ffd1578ce5e0fe0955ad7ab94228
Saeed Jannati <msaeedjannati72@gmail.com>
2025-01-07 09:47:38 +0330

    feat: add LeetCode1408StringMatchingInAnArray

---
+++ LeetCodeProblemsCpp/LeetCodeProblemsCpp/Problems/LeetCode1408StringMatchingInAnArray.cpp
@@ -0,0 +1,31 @@
+﻿#include "LeetCode1408StringMatchingInAnArray.h"
+
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
+std::vector<std::string> LeetCode1408StringMatchingInAnArray::stringMatching(std::vector<std::string>& words)
+{
+    int size = static_cast<int>(words.size());
+    std::vector<std::unordered_map<char, std::unordered_set<int>>> wordsMap;
+    wordsMap.resize(size);
+    std::ranges::sort(words, [](const std::string& first, const std::string& second)
+    {
+        return first.size() < second.size();
+    });
+    std::vector<std::string> result;
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (words[j].find(words[i]) != std::string::npos)
+            {
+                result.push_back(words[i]);
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+
---
+++ LeetCodeProblemsCpp/LeetCodeProblemsCpp/Problems/LeetCode1408StringMatchingInAnArray.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+
+class LeetCode1408StringMatchingInAnArray
+{
+private:
+    std::vector<std::string> stringMatching(std::vector<std::string>& words); 
+public:
+    
+};
