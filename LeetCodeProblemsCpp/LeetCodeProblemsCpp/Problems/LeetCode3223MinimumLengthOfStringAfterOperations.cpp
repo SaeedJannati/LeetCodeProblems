6da0578461e2da88fd9749718ef91adbe0e5d2cd
Saeed Jannati <msaeedjannati72@gmail.com>
2025-01-13 09:25:00 +0330

    feat: add LeetCode3223MinimumLengthOfStringAfterOperations

---
+++ LeetCodeProblemsCpp/LeetCodeProblemsCpp/Problems/LeetCode3223MinimumLengthOfStringAfterOperations.cpp
@@ -0,0 +1,32 @@
+﻿#include "LeetCode3223MinimumLengthOfStringAfterOperations.h"
+
+#include <unordered_map>
+
+int LeetCode3223MinimumLengthOfStringAfterOperations::minimumLength(std::string s)
+{
+    std::unordered_map<char, int> map;
+    for (auto& c : s)
+    {
+        if (!map.contains(c))
+            map[c]=1;
+        else
+            map[c]++;
+    }
+    int length=0;
+    for (auto& pair : map)
+    {
+        if (pair.second<3)
+        {
+            length+=pair.second;
+            continue;
+        }
+        if (pair.second%2==0)
+        {
+            length+=2;
+            continue;
+        }
+        length+=1;
+          
+    }
+    return length;
+}
---
+++ LeetCodeProblemsCpp/LeetCodeProblemsCpp/Problems/LeetCode3223MinimumLengthOfStringAfterOperations.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include <string>
+
+class LeetCode3223MinimumLengthOfStringAfterOperations
+{
+private:
+    int minimumLength(std::string s);
+public:
+    
+};
