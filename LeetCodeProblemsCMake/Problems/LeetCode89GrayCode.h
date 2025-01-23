532efb6f9fd25bc89700e2efc18863ef6d6ed8dc
Saeed Jannati <msaeedjannati72@gmail.com>
2025-01-23 22:12:08 +0330

    feat: add LeetCode89GrayCode !!!THIS ONE YOU SHOULD MEMORISE!!!

---
+++ LeetCodeProblemsCMake/Problems/LeetCode89GrayCode.h
@@ -0,0 +1,20 @@
+//
+// Created by saeed on 1/23/25.
+//
+
+#ifndef LEETCODE89GRAYCODE_H
+#define LEETCODE89GRAYCODE_H
+#include <vector>
+
+#include "BaseProblem.h"
+
+
+class LeetCode89GrayCode: BaseProblem {
+    std::vector<int> grayCode(int n);
+    public:
+    void Run() override;
+};
+
+
+
+#endif //LEETCODE89GRAYCODE_H
