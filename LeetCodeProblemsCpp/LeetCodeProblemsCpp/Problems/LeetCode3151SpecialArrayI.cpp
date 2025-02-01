2ad056c5c57236979904c59cd9f1f4e9e92154a4
Saeed Jannati <msaeedjannati72@gmail.com>
2025-02-01 13:04:33 +0330

    feat: add LeetCode3151SpecialArrayI

---
+++ LeetCodeProblemsCpp/LeetCodeProblemsCpp/Problems/LeetCode3151SpecialArrayI.cpp
@@ -0,0 +1,14 @@
+﻿#include "LeetCode3151SpecialArrayI.h"
+
+bool LeetCode3151SpecialArrayI::isArraySpecial(std::vector<int>& nums)
+{
+    int length = nums.size();
+    if (length<2)
+        return true;
+    for (int i=1;i<length;i++)
+    {
+        if (nums[i]%2==nums[i-1]%2)
+            return false;
+    }
+    return true;
+}
---
+++ LeetCodeProblemsCpp/LeetCodeProblemsCpp/Problems/LeetCode3151SpecialArrayI.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <vector>
+
+class LeetCode3151SpecialArrayI
+{
+public:
+    bool isArraySpecial(std::vector<int>& nums);
+};
