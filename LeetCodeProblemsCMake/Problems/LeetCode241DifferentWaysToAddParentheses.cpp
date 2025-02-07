798f1f57305d427fff6840925da8cf22999b6807
Saeed Jannati <msaeedjannati72@gmail.com>
2025-02-07 23:34:29 +0330

    refactor: remove redundancies from LeetCode241DifferentWaysToAddParentheses solution

--- LeetCodeProblemsCMake/Problems/LeetCode241DifferentWaysToAddParentheses.cpp
+++ LeetCodeProblemsCMake/Problems/LeetCode241DifferentWaysToAddParentheses.cpp
@@ -9,20 +9,20 @@
 
 #include "../Auxilary/Auxilaries.h"
 
-std::vector<int> GetAnswer(std::vector<int> &numbers, std::vector<char> &operators,std::vector<std::vector<std::vector<int>>>& dp, int beginIndex, int endIndex) {
-
+std::vector<int> GetAnswer(std::vector<int> &numbers, std::vector<char> &operators,
+                           std::vector<std::vector<std::vector<int> > > &dp, int beginIndex, int endIndex) {
     if (!dp[beginIndex][endIndex].empty()) {
         return dp[beginIndex][endIndex];
     }
     if (beginIndex == endIndex) {
-        dp[beginIndex][beginIndex]= {numbers[beginIndex]};
+        dp[beginIndex][beginIndex] = {numbers[beginIndex]};
         return dp[beginIndex][beginIndex];
     }
     for (int i = beginIndex; i < endIndex; i++) {
-        auto firstPart = GetAnswer(numbers, operators,dp, beginIndex, i);
-        auto secondPart = GetAnswer(numbers, operators,dp, i + 1, endIndex);
-        for (auto firstNum:firstPart) {
-            for (auto secondNum:secondPart) {
+        auto firstPart = GetAnswer(numbers, operators, dp, beginIndex, i);
+        auto secondPart = GetAnswer(numbers, operators, dp, i + 1, endIndex);
+        for (auto firstNum: firstPart) {
+            for (auto secondNum: secondPart) {
                 switch (operators[i]) {
                     case '+':
                         dp[beginIndex][endIndex].push_back(firstNum + secondNum);
@@ -46,10 +46,11 @@ std::vector<int> LeetCode241DifferentWaysToAddParentheses::diffWaysToCompute(std
     std::stack<int> numStack;
     int nextNum{};
     int powOfTenCounter{};
-    for (auto &c: expression) {
-        if (c >= '0' && c <= '9') {
-            numStack.push(c - '0');
-            continue;
+    for (int i = 0, e = expression.length(); i < e; i++) {
+        if (expression[i] >= '0' && expression[i] <= '9') {
+            numStack.push(expression[i] - '0');
+            if (i != e - 1)
+                continue;
         }
         nextNum = 0;
         powOfTenCounter = 0;
@@ -59,24 +60,15 @@ std::vector<int> LeetCode241DifferentWaysToAddParentheses::diffWaysToCompute(std
             numStack.pop();
         }
         numbers.push_back(nextNum);
-        operators.push_back(c);
-    }
-    if (!numStack.empty()) {
-        nextNum = 0;
-        powOfTenCounter = 0;
-        while (!numStack.empty()) {
-            nextNum += numStack.top() * std::pow(10, powOfTenCounter);
-            powOfTenCounter++;
-            numStack.pop();
-        }
-        numbers.push_back(nextNum);
+        if (i != e - 1)
+            operators.push_back(expression[i]);
     }
     int numsSize = static_cast<int>(numbers.size());
     std::vector dp(numsSize, std::vector(numsSize, std::vector<int>()));
-    return GetAnswer(numbers, operators, dp, 0, numsSize-1);
+    return GetAnswer(numbers, operators, dp, 0, numsSize - 1);
 }
 
 void LeetCode241DifferentWaysToAddParentheses::Run() {
-    std::string expression="2*3-4*5";
+    std::string expression = "2*3-4*5";
     Auxilaries::PrintVector(diffWaysToCompute(expression));
 }
