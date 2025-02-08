f8236525835f832c5f133449fe7541b4edaa9274
Saeed Jannati <msaeedjannati72@gmail.com>
2025-02-08 13:12:57 +0330

    feat: add LeetCode390EliminationGame !!with recursion!!

--- LeetCodeProblemsCMake/Problems/LeetCode390EliminationGame.cpp
+++ LeetCodeProblemsCMake/Problems/LeetCode390EliminationGame.cpp
@@ -9,21 +9,42 @@
 #include <ostream>
 #include <vector>
 
+#pragma region with recursion
+int Eliminate(int &head, int &step, bool &fromLeft, int &n) {
+    if (n == 1)
+        return head;
+    if (fromLeft || n % 2 == 1) {
+        head += step;
+    }
+    step <<= 1;
+    fromLeft = !fromLeft;
+    n >>= 1;
+    return Eliminate(head, step, fromLeft, n);
+}
 
 int LeetCode390EliminationGame::lastRemaining(int n) {
     int step = 1;
     int head = 1;
     bool fromLeft = true;
-    while (n > 1) {
-        if (fromLeft || n % 2 == 1) {
-            head += step;
-        }
-        step <<= 1;
-        fromLeft = !fromLeft;
-        n >>= 1;
-    }
-    return head;
+   return Eliminate(head, step, fromLeft, n);
 }
+#pragma  endregion
+#pragma region without recursion
+// int LeetCode390EliminationGame::lastRemaining(int n) {
+//     int step = 1;
+//     int head = 1;
+//     bool fromLeft = true;
+//     while (n > 1) {
+//         if (fromLeft || n % 2 == 1) {
+//             head += step;
+//         }
+//         step <<= 1;
+//         fromLeft = !fromLeft;
+//         n >>= 1;
+//     }
+//     return head;
+// }
+#pragma endregion
 
 void LeetCode390EliminationGame::Run() {
     int n = 123;
