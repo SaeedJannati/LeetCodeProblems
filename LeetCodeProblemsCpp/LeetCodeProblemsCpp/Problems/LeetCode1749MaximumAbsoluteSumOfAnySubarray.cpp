e87ce9e62c2e34e931f3af2125d2464e93fa935c
Saeed Jannati <msaeedjannati72@gmail.com>
2025-02-26 17:01:25 +0330

    feat: add LeetCode1749MaximumAbsoluteSumOfAnySubarray

---
+++ LeetCodeProblemsCpp/LeetCodeProblemsCpp/Problems/LeetCode1749MaximumAbsoluteSumOfAnySubarray.cpp
@@ -0,0 +1,18 @@
+﻿#include "LeetCode1749MaximumAbsoluteSumOfAnySubarray.h"
+
+#include <algorithm>
+
+int LeetCode1749MaximumAbsoluteSumOfAnySubarray::maxAbsoluteSum(std::vector<int>& nums)
+{
+    int length = nums.size();
+    long prefixSum = 0;
+    int minPrefixSum = 0;
+    int maxPrefixSum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        prefixSum += nums[i];
+        maxPrefixSum = std::max<long>(prefixSum, maxPrefixSum);
+        minPrefixSum = std::min<long>(prefixSum, minPrefixSum);
+    }
+    return maxPrefixSum - minPrefixSum;
+}
---
+++ LeetCodeProblemsCpp/LeetCodeProblemsCpp/Problems/LeetCode1749MaximumAbsoluteSumOfAnySubarray.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <vector>
+
+class LeetCode1749MaximumAbsoluteSumOfAnySubarray
+{
+private:
+    int maxAbsoluteSum(std::vector<int>& nums);
+};
