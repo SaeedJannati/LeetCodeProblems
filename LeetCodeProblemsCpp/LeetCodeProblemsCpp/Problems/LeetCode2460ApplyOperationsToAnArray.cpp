90cb0628c0343daf1c9e4bfa1399a4f7429710eb
Saeed Jannati <msaeedjannati72@gmail.com>
2025-03-01 16:11:44 +0330

    feat: add LeetCode2460ApplyOperationsToAnArray

---
+++ LeetCodeProblemsCpp/LeetCodeProblemsCpp/Problems/LeetCode2460ApplyOperationsToAnArray.cpp
@@ -0,0 +1,26 @@
+﻿#include "LeetCode2460ApplyOperationsToAnArray.h"
+
+std::vector<int> LeetCode2460ApplyOperationsToAnArray::applyOperations(std::vector<int>& nums)
+{
+    int length= static_cast<int>(nums.size());
+    for (int i=0;i<length-1;i++)
+    {
+        if (nums[i]==0)
+            continue;
+        if (nums[i]!=nums[i+1])
+            continue;
+        nums[i]*=2;
+        nums[i+1]=0;
+        i++;
+    }
+    int notZeroPointer=0;
+    for (int i=0;i<length;i++)
+    {
+        if (nums[i]!=0)
+        {
+            std::swap(nums[i],nums[notZeroPointer]);
+            notZeroPointer++;
+        }
+    }
+    return nums;
+}
---
+++ LeetCodeProblemsCpp/LeetCodeProblemsCpp/Problems/LeetCode2460ApplyOperationsToAnArray.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <vector>
+
+class LeetCode2460ApplyOperationsToAnArray
+{
+private:
+    std::vector<int> applyOperations(std::vector<int>& nums);
+};
