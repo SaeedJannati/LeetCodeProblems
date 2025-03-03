f323499dcb264af86031d9b4bd635c8500367749
Saeed Jannati <msaeedjannati72@gmail.com>
2025-03-03 08:21:28 +0330

    feat: add LeetCode2161PartitionArrayAccordingToGivenPivot

---
+++ LeetCodeProblemsCpp/LeetCodeProblemsCpp/Problems/LeetCode2161PartitionArrayAccordingToGivenPivot.cpp
@@ -0,0 +1,36 @@
+﻿#include "LeetCode2161PartitionArrayAccordingToGivenPivot.h"
+
+std::vector<int> LeetCode2161PartitionArrayAccordingToGivenPivot::pivotArray(std::vector<int>& nums, int pivot)
+{
+    int length=static_cast<int>(nums.size());
+    std::vector<int> less{};
+    less.reserve(length);
+    std::vector<int> greater{};
+    greater.reserve(length);
+    std::vector<int> equal{};
+    for (int i=0;i<length;i++)
+    {
+        if (nums[i]>pivot)
+            greater.push_back(nums[i]);
+        else if(nums[i]==pivot)
+            equal.push_back(nums[i]);
+        else
+            less.push_back(nums[i]);
+    }
+    int lessLength=static_cast<int>( less.size());
+    int greaterLength=static_cast<int>( greater.size());
+    int equalLength=static_cast<int>( equal.size());
+    for (int i=0;i<lessLength;i++)
+    {
+        nums[i]=less[i];
+    }
+    for (int i=0;i<equalLength;i++)
+    {
+        nums[i+lessLength]=pivot;
+    }
+    for (int i=0;i<greaterLength;i++)
+    {
+        nums[i+lessLength+equalLength]=greater[i];
+    }
+    return nums;
+}
---
+++ LeetCodeProblemsCpp/LeetCodeProblemsCpp/Problems/LeetCode2161PartitionArrayAccordingToGivenPivot.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <vector>
+
+class LeetCode2161PartitionArrayAccordingToGivenPivot
+{
+private:
+    std::vector<int> pivotArray(std::vector<int>& nums, int pivot);
+};
