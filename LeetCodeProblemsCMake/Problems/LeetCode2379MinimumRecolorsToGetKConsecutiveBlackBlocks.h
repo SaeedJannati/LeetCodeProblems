f454132f2aa0f89567f8ff74bd2526420d5e9dce
Saeed Jannati <msaeedjannati72@gmail.com>
2025-03-08 19:24:07 +0330

    feat: add LeetCode2379MinimumRecolorsToGetKConsecutiveBlackBlocks

---
+++ LeetCodeProblemsCMake/Problems/LeetCode2379MinimumRecolorsToGetKConsecutiveBlackBlocks.h
@@ -0,0 +1,17 @@
+//
+// Created by saeed on 3/8/25.
+//
+
+#ifndef LEETCODE2379MINIMUMRECOLORSTOGETKCONSECUTIVEBLACKBLOCKS_H
+#define LEETCODE2379MINIMUMRECOLORSTOGETKCONSECUTIVEBLACKBLOCKS_H
+#include <string>
+
+
+class LeetCode2379MinimumRecolorsToGetKConsecutiveBlackBlocks {
+private:
+    int minimumRecolors(std::string blocks, int k) ;
+};
+
+
+
+#endif //LEETCODE2379MINIMUMRECOLORSTOGETKCONSECUTIVEBLACKBLOCKS_H
