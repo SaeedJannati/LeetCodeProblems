79f8c90a7a3fc3e23a725ec7e71e46fb45e50110
Saeed Jannati <msaeedjannati72@gmail.com>
2025-03-12 09:16:57 +0330

    feat: add LeetCode2529MaximumCountOfPositiveIntegerAndNegativeInteger

---
+++ LeetCodeProblemsCpp/LeetCodeProblemsCpp/Problems/LeetCode2529MaximumCountOfPositiveIntegerAndNegativeInteger.cpp
@@ -0,0 +1,15 @@
+﻿#include "LeetCode2529MaximumCountOfPositiveIntegerAndNegativeInteger.h"
+
+int LeetCode2529MaximumCountOfPositiveIntegerAndNegativeInteger::maximumCount(std::vector<int>& nums)
+{
+    int negCount {};
+    int posCount {};
+    for (auto num : nums)
+    {
+        if (num > 0)
+            posCount++;
+        if (num < 0)
+            negCount++;
+    }
+    return std::max(posCount, negCount);
+}
---
+++ LeetCodeProblemsCpp/LeetCodeProblemsCpp/Problems/LeetCode2529MaximumCountOfPositiveIntegerAndNegativeInteger.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include <vector>
+
+class LeetCode2529MaximumCountOfPositiveIntegerAndNegativeInteger
+{
+private:
+    int maximumCount(std::vector<int>& nums);
+
+    
+};
