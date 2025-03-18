7849e65a8ef5f84f1b3a6ca6e6ce8f5aa280227f
Saeed Jannati <msaeedjannati72@gmail.com>
2025-03-18 19:15:21 +0330

    feat: add LeetCode2401LongestNiceSubarray with bitwise or

--- LeetCodeProblemsCMake/Problems/LeetCode2401LongestNiceSubarray.cpp
+++ LeetCodeProblemsCMake/Problems/LeetCode2401LongestNiceSubarray.cpp
@@ -7,47 +7,73 @@
 #include <iostream>
 #include <limits>
 #include <ostream>
+#include <bits/ranges_algo.h>
 using namespace std;
-
+#pragma region With bitwise Or
 int LeetCode2401LongestNiceSubarray::longestNiceSubarray(vector<int> &nums) {
-    int length = static_cast<int>(nums.size());
+    const int length = static_cast<int>(nums.size());
     if (length == 1)
         return 1;
     int maxLength = 1;
-    int secondPointer = 1;
-    int currentLength{};
-    bool foundContradiction = false;
+    int secondPointer {};
+    int subarrayOr{};
     for (int i = 0; i < length; i++) {
-        foundContradiction = false;
-        secondPointer = i+1;
+        secondPointer = i + 1;
+        subarrayOr = nums[i];
         while (secondPointer < length) {
-            for (int j = i; j < secondPointer; j++) {
-                if ((nums[j] & nums[secondPointer]) == 0)
-                    continue;
-                foundContradiction = true;
-                currentLength=secondPointer-i;
-                if (currentLength > maxLength) {
-                    maxLength = currentLength;
-                }
-                break;
-            }
-            if (!foundContradiction) {
+            if ((nums[secondPointer] & subarrayOr) == 0) {
+                subarrayOr |= nums[secondPointer];
                 secondPointer++;
                 continue;
             }
             break;
         }
-        if (!foundContradiction) {
-            currentLength=secondPointer-i;
-            if (currentLength > maxLength) {
-                maxLength = currentLength;
-            }
-        }
+        maxLength = ranges::max(maxLength, secondPointer - i);
     }
     return maxLength;
 }
+#pragma endregion
+#pragma region two for loops
+// int LeetCode2401LongestNiceSubarray::longestNiceSubarray(vector<int> &nums) {
+//     int length = static_cast<int>(nums.size());
+//     if (length == 1)
+//         return 1;
+//     int maxLength = 1;
+//     int secondPointer = 1;
+//     int currentLength{};
+//     bool foundContradiction = false;
+//     for (int i = 0; i < length; i++) {
+//         foundContradiction = false;
+//         secondPointer = i+1;
+//         while (secondPointer < length) {
+//             for (int j = i; j < secondPointer; j++) {
+//                 if ((nums[j] & nums[secondPointer]) == 0)
+//                     continue;
+//                 foundContradiction = true;
+//                 currentLength=secondPointer-i;
+//                 if (currentLength > maxLength) {
+//                     maxLength = currentLength;
+//                 }
+//                 break;
+//             }
+//             if (!foundContradiction) {
+//                 secondPointer++;
+//                 continue;
+//             }
+//             break;
+//         }
+//         if (!foundContradiction) {
+//             currentLength=secondPointer-i;
+//             if (currentLength > maxLength) {
+//                 maxLength = currentLength;
+//             }
+//         }
+//     }
+//     return maxLength;
+// }
+#pragma endregion
 
 void LeetCode2401LongestNiceSubarray::Run() {
-    vector<int> nums={135745088,609245787,16,2048,2097152};
-    cout<<longestNiceSubarray(nums)<<endl;
+    vector<int> nums = {135745088, 609245787, 16, 2048, 2097152};
+    cout << longestNiceSubarray(nums) << endl;
 }
