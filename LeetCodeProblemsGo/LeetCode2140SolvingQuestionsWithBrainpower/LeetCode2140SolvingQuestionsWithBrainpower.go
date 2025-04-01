13c24eab4436bff7bd72c9c754cde59151709d3c
Saeed Jannati <msaeedjannati72@gmail.com>
2025-04-01 19:14:22 +0330

    feat: add LeetCode2140SolvingQuestionsWithBrainpower Go tabulation with no recurse last one was tabulation too :) cpp one was memoisation

--- LeetCodeProblemsGo/LeetCode2140SolvingQuestionsWithBrainpower/LeetCode2140SolvingQuestionsWithBrainpower.go
+++ LeetCodeProblemsGo/LeetCode2140SolvingQuestionsWithBrainpower/LeetCode2140SolvingQuestionsWithBrainpower.go
@@ -21,3 +21,25 @@ func getPointsAt(questions [][]int, dp []int64, length int, index int) int64 {
 			getPointsAt(questions, dp, length, index+questions[index][1]+1))
 	return dp[index]
 }
+
+// tabulation with no recurse
+
+// func mostPoints(questions [][]int) int64 {
+// 	length := len(questions)
+// 	dp := make([]int64, length)
+// 	var next int64
+// 	var allowedNext int64
+// 	for i := length - 1; i >= 0; i-- {
+// 		next = 0
+// 		allowedNext = 0
+// 		if i < length-1 {
+// 			next = dp[i+1]
+// 		}
+// 		if i+questions[i][1] < length-1 {
+// 			allowedNext = dp[i+questions[i][1]+1]
+// 		}
+// 		dp[i] = max(next,
+// 			int64(questions[i][0])+allowedNext)
+// 	}
+// 	return dp[0]
+// }
