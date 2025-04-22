3e280f38b69c79ad82d4215ac58bb34e059b09fa
Saeed Jannati <msaeedjannati72@gmail.com>
2025-04-22 21:31:34 +0330

    Solve LeetCode2338CountTheNumberOfIdealArrays later!

--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -3,14 +3,14 @@
 using LeetCodeProblems.Auxilaries;
 using LeetCodeProblems.Problems;
 
-var problem = new LeetCode2179CountGoodTripletsInAnArray();
+var problem = new LeetCode2338CountTheNumberOfIdealArrays();
 
 problem.Run();
 
 
 #region ScriptNameConvertor
 
-var scriptName = "2145. Count the Hidden Sequences";
+var scriptName = "2338. Count the Number of Ideal Arrays";
 ScriptNameConvertor.PrintScriptName(scriptName);
 
 
