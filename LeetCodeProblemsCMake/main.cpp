66402da8d6752d8f8af3af441ad87d2d0372c805
Saeed Jannati <msaeedjannati72@gmail.com>
2025-05-07 20:38:03 +0330

    feat: add LeetCode3341FindMinimumTimeToReachLastRoomI Cpp

--- LeetCodeProblemsCMake/main.cpp
+++ LeetCodeProblemsCMake/main.cpp
@@ -9,24 +9,25 @@
 #include "Problems/LeetCode2799CountCompleteSubarraysInAnArray.h"
 #include "Problems/LeetCode2845CountOfInterestingSubarrays.h"
 #include "Problems/LeetCode2962CountSubarraysWhereMaxElementAppearsAtLeastKTimes.h"
+#include "Problems/LeetCode3341FindMinimumTimeToReachLastRoomI.h"
 #include "Problems/LeetCode3392CountSubarraysOfLengthThreeWithACondition.h"
 #include "Problems/LeetCode838PushDominoes.h"
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    const auto problem = new LeetCode1128NumberOfEquivalentDominoPairs();
+    const auto problem = new LeetCode3341FindMinimumTimeToReachLastRoomI();
     problem->Run();
     delete problem;
 
 
 #pragma  region  print script name
     std::cout<<'\n';
-    const std::string scriptName = "1920. Build Array from Permutation";
+    const std::string scriptName = "3341. Find Minimum Time to Reach Last Room I";
     ScriptNamePrinter::PrintScriptName(scriptName);
 #pragma endregion
 #pragma region convert to vector
-    std::string input="[[1,1],[2,2],[1,1],[1,2],[1,2],[1,1]]";
+    std::string input="[[0,4],[4,4]]";
   Auxilaries::ConvertToVector(input);
 #pragma endregion
 }
