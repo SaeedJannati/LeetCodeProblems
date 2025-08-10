94cb2050832f37011d8a88f9d27375f82822e09e
Saeed Jannati <m.jannati@emofid.com>
2025-08-10 12:31:11 +0330

    feat: add LeetCode869ReorderedPowerOf2 Go less memory but not that better

--- LeetCodeProblemsGo/LeetCode869ReorderedPowerOf2/LeetCode869ReorderedPowerOf2.go
+++ LeetCodeProblemsGo/LeetCode869ReorderedPowerOf2/LeetCode869ReorderedPowerOf2.go
@@ -38,3 +38,38 @@ func Calculate() {
 	n := 10
 	fmt.Printf("result: %t \n", reorderedPowerOf2(n))
 }
+
+//less memory but not thar different
+
+// func reorderedPowerOf2(n int) bool {
+// 	powOfTwo := 1
+// 	numMap := make([]int, 10)
+// 	getDigitsMap(n, numMap)
+// 	found := false
+// 	powOfTwoMap := make([]int, 10)
+// 	for i := 0; i < 32; i++ {
+// 		for i := 0; i <= 9; i++ {
+// 			powOfTwoMap[i] = 0
+// 		}
+// 		getDigitsMap(powOfTwo, powOfTwoMap)
+// 		found = true
+// 		for i := 0; i <= 9; i++ {
+// 			if numMap[i] != powOfTwoMap[i] {
+// 				found = false
+// 				break
+// 			}
+
+// 		}
+// 		if found {
+// 			return true
+// 		}
+// 		powOfTwo <<= 1
+// 	}
+// 	return false
+// }
+// func getDigitsMap(n int, numsMap []int) {
+// 	for n > 0 {
+// 		numsMap[n%10]++
+// 		n /= 10
+// 	}
+// }
