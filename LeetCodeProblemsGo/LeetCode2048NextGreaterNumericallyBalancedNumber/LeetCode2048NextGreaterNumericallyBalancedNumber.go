258f941d6841b12647e7f2353eef1943e8b65327
Saeed Jannati <msaeedjannati72@gmail.com>
2025-10-24 11:58:58 +0330

    feat: add LeetCode2048NextGreaterNumericallyBalancedNumber Go

---
+++ LeetCodeProblemsGo/LeetCode2048NextGreaterNumericallyBalancedNumber/LeetCode2048NextGreaterNumericallyBalancedNumber.go
@@ -0,0 +1,64 @@
+package LeetCode2048NextGreaterNumericallyBalancedNumber
+
+import (
+	"math"
+	"slices"
+)
+
+func nextBeautifulNumber(n int) int {
+	if n == 0 {
+		return 1
+	}
+	if n == 1e6 {
+		return 1_224_444
+	}
+	digits := []int{}
+	frequencies := make([]int, 0, 10)
+	num := n
+	for num > 0 {
+		digits = append(digits, num%10)
+		num /= 10
+	}
+	slices.Reverse(digits)
+	result := math.MaxInt32
+	getNum(frequencies, digits, 0, true, len(digits), &result, 0, n)
+	return result
+}
+func getNum(frequencies, digits []int, index int, shouldBeGreater bool, totalDigitCount int, minNum *int, numSofar, num int) {
+	if index == totalDigitCount {
+		addedDigit := false
+		for i := 1; i < 10; i++ {
+			if frequencies[i] == 0 {
+				continue
+			}
+			if frequencies[i] == i {
+				continue
+			}
+			if frequencies[i] == i-1 && !addedDigit {
+				numSofar *= 10
+				numSofar += i
+				addedDigit = true
+				continue
+			}
+			return
+		}
+		if numSofar > num {
+			*minNum = min(*minNum, numSofar)
+			return
+		}
+		if frequencies[1] == 0 {
+			numSofar *= 10
+			numSofar += 1
+			*minNum = min(*minNum, numSofar)
+		}
+		return
+	}
+	for i := 1; i <= totalDigitCount+1; i++ {
+		if frequencies[i] == i {
+			continue
+		}
+		frequencies[i]++
+		getNum(frequencies, digits, index+1, !shouldBeGreater || i == digits[index], totalDigitCount, minNum, numSofar*10+i, num)
+		frequencies[i]--
+	}
+}
--- LeetCodeProblemsGo/main.go
+++ LeetCodeProblemsGo/main.go
@@ -5,6 +5,6 @@ import "LeetCodeProblemsGo/LeetCode1488AvoidFloodInTheCity"
 func main() {
 
 	LeetCode1488AvoidFloodInTheCity.Calculate()
-	rawScriptName := "3347. Maximum Frequency of an Element After Performing Operations II"
+	rawScriptName := "2048. Next Greater Numerically Balanced Number"
 	printScriptName(rawScriptName)
 }
