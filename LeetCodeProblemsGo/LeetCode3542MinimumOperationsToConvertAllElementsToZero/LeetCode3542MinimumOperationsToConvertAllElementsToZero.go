6b8ae586d982b09259f313a89316175125687895
Saeed Jannati <msaeedjannati72@gmail.com>
2025-11-10 12:03:51 +0330

    feat: add LeetCode3542MinimumOperationsToConvertAllElementsToZero Go optimised approach

--- LeetCodeProblemsGo/LeetCode3542MinimumOperationsToConvertAllElementsToZero/LeetCode3542MinimumOperationsToConvertAllElementsToZero.go
+++ LeetCodeProblemsGo/LeetCode3542MinimumOperationsToConvertAllElementsToZero/LeetCode3542MinimumOperationsToConvertAllElementsToZero.go
@@ -1,35 +1,22 @@
 package LeetCode3542MinimumOperationsToConvertAllElementsToZero
 
 func minOperations(nums []int) int {
-	length := len(nums)
-	rightRange := make([]int, length)
-	for i := 0; i < length; i++ {
-		rightRange[i] = length - 1
-	}
-	monotoniqueMaxStack := [][]int{}
-	for i := length - 1; i >= 0; i-- {
+	operations := 0
+	monotoniqueMaxStack := []int{}
+	for _, num := range nums {
 		for (len(monotoniqueMaxStack) > 0) &&
-			monotoniqueMaxStack[len(monotoniqueMaxStack)-1][0] >= nums[i] {
+			monotoniqueMaxStack[len(monotoniqueMaxStack)-1] > num {
 			monotoniqueMaxStack = monotoniqueMaxStack[:len(monotoniqueMaxStack)-1]
 		}
-		if len(monotoniqueMaxStack) > 0 {
-			rightRange[i] = monotoniqueMaxStack[len(monotoniqueMaxStack)-1][1] - 1
-		}
-		monotoniqueMaxStack = append(monotoniqueMaxStack, []int{nums[i], i})
-	}
-	operationRange := make(map[int]int)
-	operations := 0
-	for i := 0; i < length; i++ {
-		if nums[i] == 0 {
+		if num == 0 {
 			continue
 		}
-		if index, exists := operationRange[nums[i]]; exists {
-			if index >= rightRange[i] {
-				continue
-			}
+		if len(monotoniqueMaxStack) == 0 || monotoniqueMaxStack[len(monotoniqueMaxStack)-1] < num {
+			monotoniqueMaxStack = append(monotoniqueMaxStack, num)
+			operations++
 		}
-		operations++
-		operationRange[nums[i]] = rightRange[i]
+
 	}
+
 	return operations
 }
