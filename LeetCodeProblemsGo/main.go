259dca354cf409665aff824ba5a1d811b549e846
Saeed Jannati <msaeedjannati72@gmail.com>
2025-11-22 14:22:16 +0330

    feat: add LeetCodeUglyNumber Go

--- LeetCodeProblemsGo/main.go
+++ LeetCodeProblemsGo/main.go
@@ -5,6 +5,6 @@ import "LeetCodeProblemsGo/LeetCode1488AvoidFloodInTheCity"
 func main() {
 
 	LeetCode1488AvoidFloodInTheCity.Calculate()
-	rawScriptName := "3190. Find Minimum Operations to Make All Elements Divisible by Three"
+	rawScriptName := "Ugly Number"
 	printScriptName(rawScriptName)
 }
