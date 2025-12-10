f1f465cdd80fdccacfb645a4c10def67d5c66296
Saeed Jannati <msaeedjannati72@gmail.com>
2025-12-10 12:00:20 +0330

    feat: add LeetCode3577CountTheNumberOfComputerUnlockingPermutations Go

---
+++ LeetCodeProblemsGo/LeetCode3577CountTheNumberOfComputerUnlockingPermutations/LeetCode3577CountTheNumberOfComputerUnlockingPermutations.go
@@ -0,0 +1,19 @@
+package LeetCode3577CountTheNumberOfComputerUnlockingPermutations
+
+func countPermutations(complexity []int) int {
+	firstItem := complexity[0]
+
+	length := len(complexity)
+	for i := 1; i < length; i++ {
+		if complexity[i] <= firstItem {
+			return 0
+		}
+	}
+	var mod int64 = 1e9 + 7
+	var result int64
+	for i := 1; i < length; i++ {
+		result *= int64(i)
+		result %= mod
+	}
+	return int(result)
+}
--- LeetCodeProblemsGo/main.go
+++ LeetCodeProblemsGo/main.go
@@ -5,6 +5,6 @@ import "LeetCodeProblemsGo/LeetCode1488AvoidFloodInTheCity"
 func main() {
 
 	LeetCode1488AvoidFloodInTheCity.Calculate()
-	rawScriptName := "1925. Count Square Sum Triples"
+	rawScriptName := "3577. Count the Number of Computer Unlocking Permutations"
 	printScriptName(rawScriptName)
 }
