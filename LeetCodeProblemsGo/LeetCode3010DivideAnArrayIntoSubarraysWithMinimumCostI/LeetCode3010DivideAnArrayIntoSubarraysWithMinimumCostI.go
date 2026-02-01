56eff795654dfde97a953d0e8e999323011b3c6a
Saeed Jannati <saeed.jannati@digikala.com>
2026-02-01 11:34:20 +0330

    feat: add LeetCode3010DivideAnArrayIntoSubarraysWithMinimumCostI Go

---
+++ LeetCodeProblemsGo/LeetCode3010DivideAnArrayIntoSubarraysWithMinimumCostI/LeetCode3010DivideAnArrayIntoSubarraysWithMinimumCostI.go
@@ -0,0 +1,9 @@
+package LeetCode3010DivideAnArrayIntoSubarraysWithMinimumCostI
+
+import (
+	"slices"
+)
+
+func minimumCost(nums []int) int {
+	slices.Sort(nums)
+}
--- LeetCodeProblemsGo/main.go
+++ LeetCodeProblemsGo/main.go
@@ -5,6 +5,6 @@ import "LeetCodeProblemsGo/LeetCode1975MaximumMatrixSum"
 func main() {
 
 	LeetCode1975MaximumMatrixSum.Calculate()
-	rawScriptName := "1458. Max Dot Product of Two Subsequences"
+	rawScriptName := "3010. Divide an Array Into Subarrays With Minimum Cost I"
 	printScriptName(rawScriptName)
 }
