83c4626fe778b85a480a110410343e4e9a51c087
Saeed Jannati <saeed.jannati@digikala.com>
2026-02-16 17:27:57 +0330

    fix: add LeetCode190ReverseBits Go

--- LeetCodeProblemsGo/LeetCode190ReverseBits/LeetCode190ReverseBits.go
+++ LeetCodeProblemsGo/LeetCode190ReverseBits/LeetCode190ReverseBits.go
@@ -1,22 +1,11 @@
 package LeetCode190ReverseBits
 
 func reverseBits(n int) int {
-	first, second := 0, 0
+	m := 0
 	for i := 0; i < 16; i++ {
-		first = 0
-		if (n & (1 << i)) > 0 {
-			first = 1
-		}
-		second = 0
-		if (n & (1 << (31 - i))) > 0 {
-			second = 1
-		}
-		first = first ^ second
-		second = first ^ second
-		first = first ^ second
-		n = (n & ^(1 << i)) | (first << i)
-		n = (n & ^(1 << (31 - i))) | (second << (31 - i))
+		m |= ((n & (1 << (31 - i))) >> (31 - i)) << i
+		m |= ((n & (1 << i)) >> i) << (31 - i)
 	}
-	return n
+	return m
 
 }
