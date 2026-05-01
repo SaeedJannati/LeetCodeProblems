9f83db8f5ca0a349cfcd2a94aa8c5a2ba050d512
Saeed Jannati <saeed.jannati@digikala.com>
2026-05-01 13:13:49 +0330

    feat: add LeetCode396RotateFunction Java

--- LeetCodeProblemsJava/src/Problems/LeetCode396RotateFunction.java
+++ LeetCodeProblemsJava/src/Problems/LeetCode396RotateFunction.java
@@ -2,10 +2,25 @@ package Problems;
 
 public class LeetCode396RotateFunction extends BaseProblem {
 
-
+    public int maxRotateFunction(int[] nums) {
+        int n = nums.length;
+        int currentSum = 0;
+        int elementsSum = nums[0];
+        for (int i = 1; i < n; i++) {
+            currentSum += i * nums[i];
+            elementsSum += nums[i];
+        }
+        int maxSum = currentSum;
+        for (int i = 1; i < n; i++) {
+            currentSum += elementsSum - n * nums[n - i];
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
 
     @Override
     public void Run() {
-
+        int[] nums = new int[]{4, 3, 2, 6};
+        IO.println(maxRotateFunction(nums));
     }
 }
