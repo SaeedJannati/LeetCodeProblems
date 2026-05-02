1cade689c91407b95fe91079d230e44ad8100085
Saeed Jannati <saeed.jannati@digikala.com>
2026-05-02 18:19:24 +0330

    fix: resolve some indentation problem

--- LeetCodeProblemsCMake/Problems/LeetCode788RotatedDigits.cpp
+++ LeetCodeProblemsCMake/Problems/LeetCode788RotatedDigits.cpp
@@ -31,45 +31,46 @@ int getNotFlippingValid(int digit) {
     return 1;
 }
 
-int dp(vector<int> &digits, unordered_set<int> &validDigits, unordered_set<int> &flippingDigits,vector<vector<vector<int>>>& memo, int pos, int tight,
+int dp(vector<int> &digits, unordered_set<int> &validDigits, unordered_set<int> &flippingDigits,
+       vector<vector<vector<int> > > &memo, int pos, int tight,
        int flipped) {
-    if (memo[pos][tight][flipped]!=-1)
+    if (memo[pos][tight][flipped] != -1)
         return memo[pos][tight][flipped];
     int upperBound = tight ? digits[pos] : 9;
     if (pos == 0) {
-        if (flipped==1) {
-            memo[pos][tight][flipped]= getFlipCount(upperBound);
+        if (flipped == 1) {
+            memo[pos][tight][flipped] = getFlipCount(upperBound);
             return memo[pos][tight][flipped];
         }
-        memo[pos][tight][flipped]= getNotFlippingValid(upperBound);
+        memo[pos][tight][flipped] = getNotFlippingValid(upperBound);
         return memo[pos][tight][flipped];
     }
     int result = 0;
-    if (tight==1) {
-        if (flipped==1) {
+    if (tight == 1) {
+        if (flipped == 1) {
             if (flippingDigits.contains(digits[pos])) {
-                result += dp(digits, validDigits, flippingDigits,memo, pos - 1, 1, 0);
+                result += dp(digits, validDigits, flippingDigits, memo, pos - 1, 1, 0);
             }
             if (validDigits.contains(digits[pos])) {
-                result += dp(digits, validDigits, flippingDigits,memo, pos - 1, 1, 1);
+                result += dp(digits, validDigits, flippingDigits, memo, pos - 1, 1, 1);
             }
         } else {
             result += (!flippingDigits.contains(digits[pos])) && validDigits.contains(digits[pos])
-                          ? dp(digits, validDigits, flippingDigits,memo, pos - 1, 1, 0)
+                          ? dp(digits, validDigits, flippingDigits, memo, pos - 1, 1, 0)
                           : 0;
         }
         upperBound = digits[pos] - 1;
     }
     if (upperBound >= 0) {
-        if (flipped==1) {
-            result += getFlipCount(upperBound) * dp(digits, validDigits, flippingDigits,memo, pos - 1, 0, 0);
+        if (flipped == 1) {
+            result += getFlipCount(upperBound) * dp(digits, validDigits, flippingDigits, memo, pos - 1, 0, 0);
             result += (getFlipCount(upperBound) + getNotFlippingValid(upperBound)) * dp(
-                digits, validDigits, flippingDigits, memo,pos - 1, 0, 1);
-        }else {
-            result += getNotFlippingValid(upperBound)* dp(digits, validDigits, flippingDigits,memo, pos - 1, 0, 0);
+                digits, validDigits, flippingDigits, memo, pos - 1, 0, 1);
+        } else {
+            result += getNotFlippingValid(upperBound) * dp(digits, validDigits, flippingDigits, memo, pos - 1, 0, 0);
         }
     }
-     memo[pos][tight][flipped]= result;
+    memo[pos][tight][flipped] = result;
     return memo[pos][tight][flipped];
 }
 
@@ -80,10 +81,11 @@ int LeetCode788RotatedDigits::rotatedDigits(int n) {
         n /= 10;
     }
     int length = static_cast<int>(digits.size());
-    vector<vector<vector<int>>> memoisation=vector<vector<vector<int>>>(length,vector<vector<int>>(2,vector<int>(2,-1)));
+    vector<vector<vector<int> > > memoisation = vector<vector<vector<int> > >(
+        length, vector<vector<int> >(2, vector<int>(2, -1)));
     unordered_set<int> flippingDigits{2, 5, 6, 9};
     unordered_set<int> validDigits{2, 5, 6, 9, 0, 1, 8};
-    return dp(digits, validDigits, flippingDigits,memoisation, length - 1, 1, 1);
+    return dp(digits, validDigits, flippingDigits, memoisation, length - 1, 1, 1);
 }
 
 void LeetCode788RotatedDigits::Run() {
