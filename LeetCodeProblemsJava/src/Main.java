5abe03538bea4bd2eac8ed590d8d334858709ccf
Saeed Jannati <saeed.jannati@digikala.com>
2026-05-10 11:31:28 +0330

    feat: add LeetCode2770MaximumNumberOfJumpsToReachTheLastIndex Java

--- LeetCodeProblemsJava/src/Main.java
+++ LeetCodeProblemsJava/src/Main.java
@@ -2,39 +2,46 @@ import Problems.*;
 
 public class Main {
     public static void main(String[] args) {
-        BaseProblem problem = new LeetCode629MinimumJumpstoReachEndviaPrimeTeleportation();
+        BaseProblem problem = new LeetCode2770MaximumNumberOfJumpsToReachTheLastIndex();
         problem.Run();
-        printScriptName("1914. Cyclically Rotating a Grid");
+        printScriptName("2770. Maximum Number of Jumps to Reach the Last Index");
         convertSquareToCurly("[[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]]");
     }
+
     private static void printScriptName(String rawScriptName) {
-         String name=convertScriptName(rawScriptName);
-         String commitName= "feat: add "+name+" Java";
-         IO.println(name);
-         IO.println(commitName);
+        String name = convertScriptName(rawScriptName);
+        String commitName = "feat: add " + name + " Java";
+        IO.println(name);
+        IO.println(commitName);
     }
+
     private static String convertScriptName(String rawScriptName) {
-           var result=new StringBuilder();
-           int rawLength=rawScriptName.length();
-           for (int i=0;i<rawLength;i++){
-               if (rawScriptName.charAt(i)!=' ' && rawScriptName.charAt(i)!='-' &&rawScriptName.charAt(i)!='.' &&
-                       rawScriptName.charAt(i)!='(' && rawScriptName.charAt(i)!=')' &&rawScriptName.charAt(i)!='[' &&
-                       rawScriptName.charAt(i)!=']' && rawScriptName.charAt(i)!='{' &&rawScriptName.charAt(i)!='}' &&
-                       rawScriptName.charAt(i)!='\'' && rawScriptName.charAt(i)!='/'
-               ){
-                    result.append(rawScriptName.charAt(i));
-               }
-           }
-        return  "LeetCode"+result;
-    }
-    private static void convertSquareToCurly(String raw) {
-        char[] chars=raw.toCharArray();
-        for(int i=0;i<raw.length();i++){
-            if(chars[i]=='['){
-                chars[i]='{';
+        var result = new StringBuilder();
+        int rawLength = rawScriptName.length();
+        for (int i = 0; i < rawLength; i++) {
+            if (rawScriptName.charAt(i) == ' ' && i + 1 < rawLength) {
+                result.append(Character.toUpperCase(rawScriptName.charAt(i + 1)));
+                ++i;
+                continue;
             }
-            else if(chars[i]==']'){
-                chars[i]='}';
+            if (rawScriptName.charAt(i) != ' ' && rawScriptName.charAt(i) != '-' && rawScriptName.charAt(i) != '.' &&
+                    rawScriptName.charAt(i) != '(' && rawScriptName.charAt(i) != ')' && rawScriptName.charAt(i) != '[' &&
+                    rawScriptName.charAt(i) != ']' && rawScriptName.charAt(i) != '{' && rawScriptName.charAt(i) != '}' &&
+                    rawScriptName.charAt(i) != '\'' && rawScriptName.charAt(i) != '/'
+            ) {
+                result.append(rawScriptName.charAt(i));
+            }
+        }
+        return "LeetCode" + result;
+    }
+
+    private static void convertSquareToCurly(String raw) {
+        char[] chars = raw.toCharArray();
+        for (int i = 0; i < raw.length(); i++) {
+            if (chars[i] == '[') {
+                chars[i] = '{';
+            } else if (chars[i] == ']') {
+                chars[i] = '}';
             }
         }
         IO.println(new String(chars));
---
+++ LeetCodeProblemsJava/src/Problems/LeetCode2770MaximumNumberOfJumpsToReachTheLastIndex.java
@@ -0,0 +1,31 @@
+package Problems;
+
+public class LeetCode2770MaximumNumberOfJumpsToReachTheLastIndex extends BaseProblem {
+    public int maximumJumps(int[] nums, int target) {
+        int length = nums.length;
+        int[] dp = new int[length];
+        dp[0] = 0;
+        for (int i = 1; i < length; i++) {
+            dp[i]=-1;
+        }
+        for (int i = 1; i < length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (Math.abs(nums[i] - nums[j]) > target) {
+                    continue;
+                }
+                if (dp[j] == -1) {
+                    continue;
+                }
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+        return dp[length - 1];
+    }
+
+    @Override
+    public void Run() {
+        int[] nums = new int[]{1, 3, 6, 4, 1, 2};
+        int k = 0;
+        IO.println(maximumJumps(nums, k));
+    }
+}
